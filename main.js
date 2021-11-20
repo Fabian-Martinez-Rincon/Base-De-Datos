04f3483f13765ff00806c372e0f2492e335f6bc6
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-11-20 16:10:51 -0300

    reset

--- main.js
+++ main.js
@@ -31,24 +31,31 @@ const database = [
 function selectElement(selector){
     return document.querySelector(selector);
 }
+//Limpiar el contenido 
+function clearResult(){
+    selectElement('.search-results').innerHTML="";
+}
 
 function getResults(){
     const search = selectElement('.searchbar').value;
-
-    for (let i = 0; i < database.length; i++){
-        if (
-            database[i].carName.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
-            database[i].carCode.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-        ){
-            selectElement('.search-results').innerHTML+=`
-            <div class = "search-results-item">
-                <span class= "search-item">${database[i].carName}</span>
-                <span class= "search-item">${database[i].carPrice}</span>
-                <span class= "search-item">${database[i].carCode}</span>
-            </div>
-            `;
+    clearResult();
+    if(search.length > 0){
+        for (let i = 0; i < database.length; i++){
+            if (
+                database[i].carName.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+                database[i].carCode.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+            ){
+                selectElement('.search-results').innerHTML+=`
+                <div class = "search-results-item">
+                    <span class= "search-item">${database[i].carName}</span>
+                    <span class= "search-item">${database[i].carPrice}</span>
+                    <span class= "search-item">${database[i].carCode}</span>
+                </div>
+                `;
+            }
         }
     }
+    
 }
 
 selectElement('.searchbar').addEventListener('keyup', getResults);
